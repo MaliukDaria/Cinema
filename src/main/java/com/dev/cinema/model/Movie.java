a49ce9afc217ba1f6a38997d6f4c1773159b7c6c
MaliukDaria <maliuk.dasha@gmail.com>
2020-09-30 16:52:56 +0300

    fixed checkstyle

--- src/main/java/com/dev/cinema/model/Movie.java
+++ src/main/java/com/dev/cinema/model/Movie.java
@@ -1,9 +1,12 @@
 package com.dev.cinema.model;
 
-import com.sun.istack.NotNull;
-
 import java.util.Objects;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "movie")
