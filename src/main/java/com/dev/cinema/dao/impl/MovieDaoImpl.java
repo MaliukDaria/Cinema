9b807d79bc83cd97073556873753ab8a96c331b6
Dasha <68428976+MaliukDaria@users.noreply.github.com>
2020-10-01 11:00:59 +0300

    implemented Hw 1 (#1)
    * add all necessary classes
    
    * implemented hibernate connection
    
    * added empty line in the end of hibernate.cfg.xml file
    
    * removed id set; explicitly indicated the port
    
    * added not null constraint into title column
    
    * fixed checkstyle

---
+++ src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
@@ -0,0 +1,47 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.MovieDao;
+import com.dev.cinema.exception.DataProcessingException;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.Movie;
+import com.dev.cinema.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+            return movie;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException(
+                    "Cant add movie " + movie.getTitle() + " to the database", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Movie> getAllMoviesQuery = session.createQuery("FROM Movie", Movie.class);
+            return getAllMoviesQuery.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Cant get all movies from the database", e);
+        }
+    }
+}
