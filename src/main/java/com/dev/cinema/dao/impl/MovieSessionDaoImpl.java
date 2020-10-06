97c0eaf452f4788db1a6b13803a5eb52ea9dd61c
MaliukDaria <maliuk.dasha@gmail.com>
2020-10-06 17:01:07 +0300

    removed unnecessary variable

--- src/main/java/com/dev/cinema/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/MovieSessionDaoImpl.java
@@ -6,7 +6,7 @@ import com.dev.cinema.lib.Dao;
 import com.dev.cinema.model.MovieSession;
 import com.dev.cinema.util.HibernateUtil;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -41,20 +41,16 @@ public class MovieSessionDaoImpl implements MovieSessionDao {
     @Override
     public List<MovieSession> getByMovieAndDate(Long movieId, LocalDate date) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            LocalDateTime start = date.atStartOfDay();
-            LocalDateTime end = start.plusDays(1).minusMinutes(1);
             Query<MovieSession> movieSessionQuery =
-                    session.createQuery(
-                            "SELECT ms "
-                                    + "FROM MovieSession ms "
+                    session.createQuery("FROM MovieSession ms "
                                     + "JOIN FETCH ms.cinemaHall "
                                     + "JOIN FETCH ms.movie "
                                     + "WHERE ms.movie.id = :id "
                                     + "AND ms.showTime BETWEEN :start AND :end",
                             MovieSession.class)
                     .setParameter("id", movieId)
-                    .setParameter("start", start)
-                    .setParameter("end", end);
+                    .setParameter("start", date.atStartOfDay())
+                    .setParameter("end", date.atTime(LocalTime.MAX));
             return movieSessionQuery.getResultList();
         } catch (Exception e) {
             throw new DataProcessingException("Cant get movie session by movie and date", e);
