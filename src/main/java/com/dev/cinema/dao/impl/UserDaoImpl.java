6eb6b4f176a4c77b58a973d3127400b7ddc83e8e
MaliukDaria <maliuk.dasha@gmail.com>
2020-10-07 16:09:07 +0300

    implemented: user model, UserDAO and UserService, AuthenticationService, HashUtil

---
+++ src/main/java/com/dev/cinema/dao/impl/UserDaoImpl.java
@@ -0,0 +1,50 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.UserDao;
+import com.dev.cinema.exception.DataProcessingException;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.User;
+import com.dev.cinema.util.HibernateUtil;
+import java.util.Optional;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User add(User user) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException(
+                    "Cant add user with email: " + user.getEmail()
+                            + " to the database", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<User> findByEmailQuery = session.createQuery(
+                    "FROM User WHERE email = :userEmail", User.class)
+                    .setParameter("userEmail", email);
+            return Optional.ofNullable(findByEmailQuery.uniqueResult());
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find user with email: " + email, e);
+        }
+    }
+}
---
+++ src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
@@ -0,0 +1,36 @@
+package com.dev.cinema.security;
+
+import com.dev.cinema.exception.AuthenticationException;
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.lib.Service;
+import com.dev.cinema.model.User;
+import com.dev.cinema.service.UserService;
+import com.dev.cinema.util.HashUtil;
+import java.util.Optional;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> user = userService.findByEmail(email);
+        if (user.isPresent()
+                && user.get().getPassword()
+                .equals(HashUtil.hashPassword(password, user.get().getSalt()))) {
+            return user.get();
+        } else {
+            throw new AuthenticationException("Incorrect login or password");
+        }
+    }
+
+    @Override
+    public User register(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setSalt(HashUtil.getSalt());
+        user.setPassword(HashUtil.hashPassword(password, user.getSalt()));
+        return userService.add(user);
+    }
+}
