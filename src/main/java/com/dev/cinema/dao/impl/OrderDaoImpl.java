1faaf9cf4c6abda42bfa90e1548d003b36df7d43
MaliukDaria <maliuk.dasha@gmail.com>
2020-10-15 15:13:02 +0300

    added GenericDaoImpl; added logger

--- src/main/java/com/dev/cinema/dao/impl/OrderDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/OrderDaoImpl.java
@@ -1,42 +1,16 @@
 package com.dev.cinema.dao.impl;
 
 import com.dev.cinema.dao.OrderDao;
-import com.dev.cinema.exception.DataProcessingException;
 import com.dev.cinema.lib.Dao;
 import com.dev.cinema.model.Order;
 import com.dev.cinema.model.User;
 import com.dev.cinema.util.HibernateUtil;
 import java.util.List;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 @Dao
-public class OrderDaoImpl implements OrderDao {
-    @Override
-    public Order add(Order order) {
-        Transaction transaction = null;
-        Session session = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-            session.save(order);
-            transaction.commit();
-            return order;
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            throw new DataProcessingException(
-                    "Cant add order with user id: " + order.getUser().getId()
-                            + " to the database", e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-    }
-
+public class OrderDaoImpl extends GenericDaoImpl<Order> implements OrderDao {
     @Override
     public List<Order> getUserOrders(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
