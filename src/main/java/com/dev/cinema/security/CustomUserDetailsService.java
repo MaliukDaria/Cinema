8e52ab83810c95e32286181f2b954f05d6069b98
MaliukDaria <maliuk.dasha@gmail.com>
2020-11-03 14:56:31 +0200

    used orElseThrow instead of if-else construction in CustomUserDetailsService class

--- src/main/java/com/dev/cinema/security/CustomUserDetailsService.java
+++ src/main/java/com/dev/cinema/security/CustomUserDetailsService.java
@@ -2,7 +2,6 @@ package com.dev.cinema.security;
 
 import com.dev.cinema.model.User;
 import com.dev.cinema.service.UserService;
-import java.util.Optional;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,19 +18,15 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Optional<User> optionalUser = userService.findByEmail(email);
-        if (optionalUser.isPresent()) {
-            User user = optionalUser.get();
-            UserBuilder builder =
-                    org.springframework.security.core.userdetails.User.withUsername(email);
-            builder.password(user.getPassword());
-            String[] userRoles = user.getRoles().stream()
-                    .map(r -> r.getRoleName().toString())
-                    .toArray(String[]::new);
-            builder.roles(userRoles);
-            return builder.build();
-        } else {
-            throw new UsernameNotFoundException("User not found");
-        }
+        User user = userService.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        UserBuilder builder =
+                org.springframework.security.core.userdetails.User.withUsername(email);
+        builder.password(user.getPassword());
+        String[] userRoles = user.getRoles().stream()
+                .map(r -> r.getRoleName().toString())
+                .toArray(String[]::new);
+        builder.roles(userRoles);
+        return builder.build();
     }
 }
